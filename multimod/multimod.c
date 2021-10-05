15770b77a6a469ae48e750146ddeccb6f2919a0a
tracer-nju <tracer@nju.edu.cn>
2021-10-06 00:48:45 +0800

    Static hostname: siheng-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 88452b4c23fc454ca7a5f94490554f29            Boot ID: 83bb205e03b74056b3fcab7b7352c89e     Virtualization: oracle   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  00:48:45 up 11:47,  1 user,  load average: 0.58, 0.33, 0.19

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,27 +1,20 @@
 #include <stdint.h>
 
-static inline uint64_t multimod2(uint64_t b, uint64_t m){
-  while(b>=m){
-    b-=m;
-  }
-  return b;
-}
-
 static inline uint64_t add_mult(uint64_t a, uint64_t b, uint64_t m){
   //a=multimod2(a,m);
   //b=multimod2(b,m);
   if((a+b)<a){
-    uint64_t tmp1=multimod2(a+b+1,m);
-    uint64_t tmp2=multimod2(-1ULL,m);
+    uint64_t tmp1=(a+b+1)%m;
+    uint64_t tmp2=(-1ULL)%m;
     return add_mult(tmp1,tmp2,m);
   }else{
-    return multimod2(a+b,m);
+    return (a+b)%m;
   }
 }
       
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  a=multimod2(a,m);
-  b=multimod2(b,m);
+  a=a%m;
+  b=b%m;
   uint64_t result=0;
   for(int i = 0; i < 64;i++){
     uint64_t tmp1 = a & 1;
