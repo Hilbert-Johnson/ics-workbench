0340edb225d4f184494d98a6ebb9eb949765536d
tracer-nju <tracer@nju.edu.cn>
2021-10-06 20:52:23 +0800

    Static hostname: siheng-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 88452b4c23fc454ca7a5f94490554f29            Boot ID: 83bb205e03b74056b3fcab7b7352c89e     Virtualization: oracle   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:52:23 up 18:26,  1 user,  load average: 0.55, 0.39, 0.28

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,18 +1,25 @@
 #include <stdint.h>
 
+static inline uint64_t mul2(uint64_t a, uint64_t m){
+  while(a>=m){
+    a-=m;
+  }
+  return a;
+}
+
 static inline uint64_t add_mult(uint64_t a, uint64_t b, uint64_t m){
   if((a+b)<a){
-    uint64_t tmp1=(a+b+1)&(m-1);
-    uint64_t tmp2=(-1ULL)&(m-1);
+    uint64_t tmp1=mul2(a+b+1,m);
+    uint64_t tmp2=mul2(-1ULL,m);
     return add_mult(tmp1,tmp2,m);
   }else{
-    return (a+b)&(m-1);
+    return mul2(a+b,m);
   }
 }
       
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  a=a&(m-1);
-  b=b&(m-1);
+  a=mul2(a,m);
+  b=mul2(b,m);
   uint64_t result=0;
   for(int i = 0; i < 64;i++){
     uint64_t tmp1 = a & 1;
