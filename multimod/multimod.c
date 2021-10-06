e3985f0c29190dd9e9ef0625c6cecc669379b158
tracer-nju <tracer@nju.edu.cn>
2021-10-06 20:03:54 +0800

    Static hostname: siheng-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 88452b4c23fc454ca7a5f94490554f29            Boot ID: 83bb205e03b74056b3fcab7b7352c89e     Virtualization: oracle   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:03:54 up 17:41,  1 user,  load average: 0.21, 0.19, 0.16

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,20 +1,18 @@
 #include <stdint.h>
 
 static inline uint64_t add_mult(uint64_t a, uint64_t b, uint64_t m){
-  //a=multimod2(a,m);
-  //b=multimod2(b,m);
   if((a+b)<a){
-    uint64_t tmp1=(a+b+1)%m;
-    uint64_t tmp2=(-1ULL)%m;
+    uint64_t tmp1=(a+b+1)&(m-1);
+    uint64_t tmp2=(-1ULL)&(m-1);
     return add_mult(tmp1,tmp2,m);
   }else{
-    return (a+b)%m;
+    return (a+b)&(m-1);
   }
 }
       
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  a=a%m;
-  b=b%m;
+  a=a&(m-1);
+  b=b&(m-1);
   uint64_t result=0;
   for(int i = 0; i < 64;i++){
     uint64_t tmp1 = a & 1;
