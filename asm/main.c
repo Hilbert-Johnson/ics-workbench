fd33552f1fbba8713f29387128cdc39b37936bd3
tracer-nju <tracer@nju.edu.cn>
2021-10-28 21:02:01 +0800

    Static hostname: siheng-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 88452b4c23fc454ca7a5f94490554f29            Boot ID: a6f9ab7b47b54e4e93cbdcba525cf32d     Virtualization: oracle   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  21:02:01 up 11:19,  1 user,  load average: 0.23, 0.29, 0.27

--- asm/main.c
+++ asm/main.c
@@ -7,9 +7,6 @@ asm_jmp_buf buf;
 int main() {
   int r = asm_setjmp(buf);
   printf("r:%d\n",r);
-  for(int i = 0;i<8;i++){
-    printf("0x%lx\n",buf[0].callee_saved[i]);
-  }
   if (r == 0) {
     //assert(asm_add(1234, 5678) == 6912);
     //assert(asm_popcnt(0x0123456789abcdefULL) == 32);
