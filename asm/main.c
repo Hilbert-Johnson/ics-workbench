13e0509bcebf22bd2c97c5c575dd4956bc61d9d1
tracer-nju <tracer@nju.edu.cn>
2021-10-28 14:47:46 +0800

    Static hostname: siheng-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 88452b4c23fc454ca7a5f94490554f29            Boot ID: a6f9ab7b47b54e4e93cbdcba525cf32d     Virtualization: oracle   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  14:47:46 up  7:06,  1 user,  load average: 0.52, 0.26, 0.19

--- asm/main.c
+++ asm/main.c
@@ -10,8 +10,8 @@ int main() {
   int r = setjmp(buf);
   printf("r:%d\n",r);
   if (r == 0) {
-    //assert(asm_add(1234, 5678) == 6912);
-    //assert(asm_popcnt(0x0123456789abcdefULL) == 32);
+    assert(asm_add(1234, 5678) == 6912);
+    assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     // TODO: add more tests here.
     longjmp(buf, 123);
     printf("do not reach here\n");
