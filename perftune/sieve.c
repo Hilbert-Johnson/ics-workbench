f47624e9bdde936258bf9d6f84d77a66454bbd00
tracer-nju <tracer@nju.edu.cn>
2021-11-25 23:20:10 +0800

    Static hostname: siheng-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 88452b4c23fc454ca7a5f94490554f29            Boot ID: 56a3885244264aa387f79c0021750dae     Virtualization: oracle   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:20:10 up 1 day, 2 min,  1 user,  load average: 0.35, 0.24, 0.15

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -10,17 +10,18 @@ static int  primes[N];
 
 int *sieve(int n) {
   assert(n + 1 < N);
-  for (int i = 0; i <= n; i++)
+  for (int i = 3; i <= n; i+=2)
     is_prime[i] = true;
 
-  for (int i = 2; i <= n; i++) {
-    for (int j = i + i; j <= n; j += i) {
+  for (int i = 3; i <= n; i+=2) {
+    for (int j = i+i<<2; j <= n; j += (i<<2)) {
       is_prime[j] = false;
     }
   }
 
   int *p = primes;
-  for (int i = 2; i <= n; i++)
+  *p++ = 2;
+  for (int i = 3; i <= n; i+=2)
     if (is_prime[i]) {
       *p++ = i;
     }
