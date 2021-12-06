174c52d5f4f11d3ba1761b01685d667e9b57cd61
tracer-nju <tracer@nju.edu.cn>
2021-12-06 19:33:39 +0800

    Static hostname: siheng-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 88452b4c23fc454ca7a5f94490554f29            Boot ID: aa3d6c3dee7e4fb296e02087106116f6     Virtualization: oracle   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  19:33:40 up  3:09,  1 user,  load average: 0.27, 0.22, 0.19

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -5,6 +5,19 @@ void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
 
 static uint64_t cycle_cnt = 0;
+uint64_t block_num = 0;
+uint64_t associate = 0;
+
+int tag = 0;
+int index = 0;
+
+struct line{
+  bool valid,dirty;
+  uint8_t tag;
+  uint8_t memory[64];
+};
+
+struct line *chache=NULL;
 
 void cycle_increase(int n) { cycle_cnt += n; }
 
@@ -18,6 +31,11 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
 }
 
 void init_cache(int total_size_width, int associativity_width) {
+  block_num = 1 << (total_size_width - 6);
+  associate = 1 << associativity_width;
+  index = total_size_width - 6 - associativity_width;
+  tag = sizeof(uintptr_t)*8 - 6 - index;
+  chache = malloc(sizeof(struct line) * block_num);
 }
 
 void display_statistic(void) {
