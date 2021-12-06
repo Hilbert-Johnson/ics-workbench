f615fca29417dcbdf4ed4070cecbff52b4e6943f
tracer-nju <tracer@nju.edu.cn>
2021-12-06 21:32:35 +0800

    Static hostname: siheng-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 88452b4c23fc454ca7a5f94490554f29            Boot ID: aa3d6c3dee7e4fb296e02087106116f6     Virtualization: oracle   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  21:32:35 up  5:04,  1 user,  load average: 0.04, 0.19, 0.17

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -8,8 +8,8 @@ static uint64_t cycle_cnt = 0;
 uint64_t block_num = 0;
 uint64_t associate = 0;
 
-int tag = 0;
-int cache_index = 0;
+int tag_bit = 0;
+int max_index_bit = 0;
 
 struct line{
   bool valid,dirty;
@@ -21,20 +21,53 @@ struct line *cache=NULL;
 
 void cycle_increase(int n) { cycle_cnt += n; }
 
-// TODO: implement the following functions
+#define BLOCK_MASK 0x3f
 
 uint32_t cache_read(uintptr_t addr) {
-  return 0;
+  uintptr_t in_block_addr = addr & BLOCK_MASK;
+  uintptr_t index_addr = (addr<<tag_bit)>>(tag_bit+6);
+  uintptr_t tag_addr = addr>>(max_index_bit+6);
+
+  for(int i = 0; i<associate; i++){
+    if(cache[associate*index_addr + i].valid==true && cache[associate*index_addr + i].tag==tag_addr){
+      return cache[associate*index_addr + i].memory[in_block_addr];
+    }
+  }
+
+  uint8_t buf[64];
+  mem_read(addr>>6, buf);
+
+  for(int i = 0; i<associate; i++){
+    if(cache[associate*index_addr + i].valid==false){
+      cache[associate*index_addr + i].valid = true;
+      cache[associate*index_addr + i].dirty = false;
+      cache[associate*index_addr + i].tag = tag_addr;
+      for(int j = 0; j<64; j++) cache[associate*index_addr + i].memory[j] = buf[j];
+      return buf[in_block_addr];
+    }
+  }
+
+  int replace = rand() % associate;
+  if(cache[associate*index_addr + replace].dirty == true)
+    mem_write((cache[associate*index_addr + replace].tag<<max_index_bit)|index_addr, cache[associate*index_addr + replace].memory);
+  cache[associate*index_addr + replace].valid = true;
+  cache[associate*index_addr + replace].dirty = false;
+  cache[associate*index_addr + replace].tag = tag_addr;
+  for(int i=0; i<64; i++) cache[associate*index_addr + replace].memory[i] = buf[i];
+  return buf[in_block_addr];
 }
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
+  uintptr_t in_block_addr = addr & BLOCK_MASK;
+  uintptr_t index_addr = (addr<<tag_bit)>>(tag_bit+6);
+  uintptr_t tag_addr = addr>>(max_index_bit+6);
 }
 
 void init_cache(int total_size_width, int associativity_width) {
   block_num = 1 << (total_size_width - 6);
   associate = 1 << associativity_width;
-  cache_index = total_size_width - 6 - associativity_width;
-  tag = sizeof(uintptr_t)*8 - 6 - cache_index;
+  max_index_bit = total_size_width - 6 - associativity_width;
+  tag_bit = sizeof(uintptr_t)*8 - 6 - max_index_bit;
   cache = malloc(sizeof(struct line) * block_num);
   for(int i = 0; i < block_num;i++){
     cache[i].valid = false;
